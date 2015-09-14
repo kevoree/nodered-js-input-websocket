fb49eaf8f5025ef83dfabc822261262f77760db1
Maxime Tricoire <max.tricoire@gmail.com>
2015-09-14 16:59:11 +0200

    update msg conversion using nodered-msg-converter

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,14 @@ module.exports = function(grunt) {
     // retrieve your project package.json
     pkg: grunt.file.readJSON('package.json'),
 
+    kevoree: {
+      options: {
+        mergeLocalLibraries: [
+          '../../../output/websocket/js'
+        ]
+      }
+    },
+
     // creates kevlib.json which represents your project Kevoree model
     // by parsing your pkg.main entry point
     kevoree_genmodel: {
